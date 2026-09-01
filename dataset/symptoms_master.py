fbe7df47631adaeb200dd04b15af51eeb8331310
Jagannatha-dev <pjagannathajaggu@gmail.com>
2026-09-01 11:04:16 +0530

    Add files via upload

---
+++ dataset/symptoms_master.py
@@ -0,0 +1,301 @@
+SYMPTOMS = [
+    # ===========================
+    # General
+    # ===========================
+    "fever",
+    "high_fever",
+    "low_grade_fever",
+    "chills",
+    "night_sweats",
+    "fatigue",
+    "weakness",
+    "malaise",
+    "loss_of_appetite",
+    "weight_loss",
+    "weight_gain",
+    "dehydration",
+    "excessive_sweating",
+
+    # ===========================
+    # Head & Neurological
+    # ===========================
+    "headache",
+    "migraine",
+    "dizziness",
+    "vertigo",
+    "confusion",
+    "memory_loss",
+    "difficulty_concentrating",
+    "loss_of_consciousness",
+    "seizures",
+    "tremors",
+    "hand_tremor",
+    "slurred_speech",
+    "difficulty_speaking",
+    "difficulty_writing",
+    "difficulty_reading",
+    "loss_of_balance",
+    "poor_balance",
+    "paralysis",
+    "muscle_spasms",
+    "numbness",
+    "tingling_hands",
+    "tingling_feet",
+
+    # ===========================
+    # Eyes
+    # ===========================
+    "blurred_vision",
+    "double_vision",
+    "vision_loss",
+    "eye_pain",
+    "red_eyes",
+    "watery_eyes",
+    "dry_eyes",
+    "light_sensitivity",
+
+    # ===========================
+    # ENT
+    # ===========================
+    "ear_pain",
+    "hearing_loss",
+    "ringing_in_ears",
+    "runny_nose",
+    "nasal_congestion",
+    "nose_bleeding",
+    "loss_of_smell",
+    "loss_of_taste",
+    "sore_throat",
+    "hoarseness",
+    "difficulty_swallowing",
+    "mouth_ulcers",
+    "bleeding_gums",
+    "bad_breath",
+
+    # ===========================
+    # Respiratory
+    # ===========================
+    "dry_cough",
+    "productive_cough",
+    "persistent_cough",
+    "coughing_blood",
+    "shortness_of_breath",
+    "difficulty_breathing",
+    "rapid_breathing",
+    "wheezing",
+    "chest_tightness",
+
+    # ===========================
+    # Cardiovascular
+    # ===========================
+    "chest_pain",
+    "palpitations",
+    "rapid_heartbeat",
+    "slow_heartbeat",
+    "high_blood_pressure",
+    "low_blood_pressure",
+    "swollen_legs",
+    "cold_hands",
+    "cold_feet",
+    "cyanosis",
+
+    # ===========================
+    # Gastrointestinal
+    # ===========================
+    "abdominal_pain",
+    "upper_abdominal_pain",
+    "lower_abdominal_pain",
+    "abdominal_tenderness",
+    "abdominal_swelling",
+    "bloating",
+    "gas",
+    "heartburn",
+    "acid_reflux",
+    "indigestion",
+    "difficulty_digesting_food",
+    "stomach_cramps",
+    "nausea",
+    "vomiting",
+    "persistent_vomiting",
+    "vomiting_blood",
+    "coffee_ground_vomit",
+    "diarrhea",
+    "bloody_diarrhea",
+    "constipation",
+    "blood_in_stool",
+    "black_stool",
+    "rectal_bleeding",
+
+    # ===========================
+    # Liver
+    # ===========================
+    "jaundice",
+    "yellow_eyes",
+    "dark_urine",
+    "pale_stool",
+    "liver_enlargement",
+
+    # ===========================
+    # Kidney & Urinary
+    # ===========================
+    "frequent_urination",
+    "painful_urination",
+    "burning_urination",
+    "blood_in_urine",
+    "cloudy_urine",
+    "difficulty_urinating",
+    "reduced_urine_output",
+    "urinary_incontinence",
+    "flank_pain",
+    "kidney_pain",
+
+    # ===========================
+    # Endocrine
+    # ===========================
+    "polyuria",
+    "polydipsia",
+    "polyphagia",
+    "excessive_thirst",
+    "excessive_hunger",
+    "cold_intolerance",
+    "heat_intolerance",
+
+    # ===========================
+    # Musculoskeletal
+    # ===========================
+    "joint_pain",
+    "joint_swelling",
+    "joint_stiffness",
+    "muscle_pain",
+    "muscle_weakness",
+    "bone_pain",
+    "back_pain",
+    "neck_pain",
+    "shoulder_pain",
+    "hip_pain",
+    "knee_pain",
+    "ankle_pain",
+    "difficulty_walking",
+
+    # ===========================
+    # Skin
+    # ===========================
+    "skin_rash",
+    "itching",
+    "red_skin",
+    "skin_blisters",
+    "skin_ulcers",
+    "dry_skin",
+    "dry_skin_patch",
+    "peeling_skin",
+    "hives",
+    "eczema_like_rash",
+    "psoriasis_like_plaques",
+    "skin_abscess",
+    "infected_wound",
+    "skin_nodules",
+    "yellow_skin",
+
+    # ===========================
+    # Hair & Nails
+    # ===========================
+    "hair_loss",
+    "hair_thinning",
+    "brittle_nails",
+    "nail_discoloration",
+
+    # ===========================
+    # Lymphatic
+    # ===========================
+    "swollen_lymph_nodes",
+    "neck_swelling",
+    "armpit_swelling",
+    "groin_swelling",
+
+    # ===========================
+    # Women's Health
+    # ===========================
+    "irregular_periods",
+    "missed_periods",
+    "heavy_menstrual_bleeding",
+    "painful_periods",
+    "pelvic_pain",
+    "pelvic_pressure",
+    "abnormal_vaginal_bleeding",
+    "vaginal_discharge",
+    "pain_during_intercourse",
+    "breast_lump",
+    "breast_pain",
+    "nipple_discharge",
+
+    # ===========================
+    # Men's Health
+    # ===========================
+    "testicular_pain",
+    "testicular_swelling",
+    "erectile_dysfunction",
+    "penile_discharge",
+
+    # ===========================
+    # Psychological
+    # ===========================
+    "anxiety",
+    "depression",
+    "panic_attacks",
+    "insomnia",
+    "hallucinations",
+    "mood_swings",
+    "irritability",
+
+    # ===========================
+    # Cancer / Chronic Disease
+    # ===========================
+    "persistent_fatigue",
+    "persistent_pain",
+    "non_healing_wound",
+    "unusual_bleeding",
+    "exercise_intolerance",
+    "easy_fatigue",
+
+    # ===========================
+    # Infection
+    # ===========================
+    "pus_discharge",
+    "oral_thrush",
+    "genital_ulcers"
+    # Additional Valid Symptoms
+"recurrent_infections",
+"slow_healing_wounds",
+"muscle_cramps",
+"tongue_biting",
+"facial_drooping",
+"personality_changes",
+"aggressive_behavior",
+"facial_swelling",
+"itchy_scalp",
+"acne",
+"leg_swelling",
+"recurrent_infections",
+# ===== Additional Symptoms =====
+"difficulty_sleeping",
+"skin_discoloration",
+"change_in_bowel_habits",
+"abdominal_mass",
+"easy_bruising",
+"frequent_infections",
+"dry_mouth",
+"tooth_pain",
+"eye_discharge",
+"voice_loss",
+"leg_cramps",
+"snoring",
+"pica",
+"pale_skin",
+"excessive_bleeding",
+"abdominal_fluid_accumulation",
+"persistent_fever",
+"persistent_diarrhea",
+"abnormal_movements",
+"swollen_joints",
+"brain_fog",
+]
